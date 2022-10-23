7d254414631e125af2e149f857bd49fa78a35442
Quinn O'Neal <quinn.oneal@mykenzie.snhu.edu>
2022-10-22 15:23:58 -1000

    Updated index html template to include repo link, added documentation for lyric formatter method

--- src/main/java/io/favword/lyrics/LyricFormatter.java
+++ src/main/java/io/favword/lyrics/LyricFormatter.java
@@ -6,12 +6,14 @@ import java.util.Arrays;
 public class LyricFormatter {
     private static final int MAX_STRING_LENGTH = 12000;
 
-    // takes in raw unformatted lyrics
-    // removes all unnecessary song annotations such as [Bridge], [Chorus], [Verse], from string
-    // removes all line breaks, empty lines, punctuation
-    // returns string of all words separated by a space
+    /**
+     * Takes in raw unformatted lyrics & removes all unnecessary song annotations such as [Bridge], [Chorus], [Verse],
+     * etc. from string. Removes all line breaks, empty lines, and punctuation.
+     * @param unformattedLyrics raw unformatted lyrics
+     * @return String of all words from lyrics, separated by a space
+     */
     public static String splitLyrics(String unformattedLyrics) {
-        //
+        // initialize arraylist with raw lyrics string split by each line break (first format step)
         ArrayList<String> list = new ArrayList<>(Arrays.asList(unformattedLyrics.split("\\n")));
         for (int i = 0; i < list.size(); i++) {
             String temp = list.get(i);
@@ -25,6 +27,7 @@ public class LyricFormatter {
             }
         }
         String temp = String.join(" ", list);
+        // check if string is longer than url character limit to prevent http 414 error code
         if (temp.length() > MAX_STRING_LENGTH) {
             return temp.substring(0, MAX_STRING_LENGTH);
         }
